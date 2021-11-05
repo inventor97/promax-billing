bad28c1b61434ea02e34b04c56374fbdb40200c5
inventor97 <mr.inventor97@gmail.com>
2021-11-06 04:48:23 +0500

    init

--- src/main/java/com/inventor/controllers/mainCtrl.java
+++ src/main/java/com/inventor/controllers/mainCtrl.java
@@ -1,4 +1,15 @@
 package com.inventor.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class mainCtrl {
+
+
+    @FXML
+    private AnchorPane mainPage;
+
+    @FXML
+    private VBox leftToolBar;
 }
